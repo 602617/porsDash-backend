2896c498d1427f25f56ea2456653a9057c158693
Martin <martin.lenes@lyse.net>
2025-06-18 23:41:50 +0200

    added register user

--- src/main/java/com/martin/demo/auth/AuthController.java
+++ src/main/java/com/martin/demo/auth/AuthController.java
@@ -1,12 +1,15 @@
 package com.martin.demo.auth;
 
 import com.martin.demo.component.JwtUtil;
+import com.martin.demo.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +29,16 @@ public class AuthController {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
+    @Autowired
+    private AppUserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AppUserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest request) {
         System.out.println(">> Trying to authenticate: " + request.getUsername());
@@ -42,5 +55,21 @@ public class AuthController {
         System.out.println(">> JWT token generated: " + token);
 
         return ResponseEntity.ok(Map.of("token", token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+        }
+
+        AppUser user = new AppUser();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode((request.getPassword())));
+        user.setRole(request.getRole());
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User registered successfully");
     }
 }
